863dad3d4a030e6ddd334664573fe50ef459656c
warsame <warsame.abdiwakil@gmail.com>
2022-02-16 12:05:41 +0200

    React App putsattu

---
+++ osa1/src/App.js
@@ -0,0 +1,25 @@
+import React from 'react';
+
+function App() {
+  return (
+    <section className="App">
+     <dl className="eka">
+      <dt><strong>Maria</strong></dt>
+      <dt>Title: CEO</dt>
+      <dt>Age:29</dt>
+      </dl>
+     <dl className="eka">
+     <dt><strong>kati</strong></dt>
+      <dt>Title: developer</dt>
+      <dt>Age:25</dt>
+      </dl>
+     <dl className="eka">
+     <dt><strong>karin</strong></dt>
+      <dt>Title: designer</dt>
+      <dt>Age:45</dt>
+     </dl>
+    </section>
+  );
+}
+
+export default App;
---
+++ osa4/src/components/Form.js
@@ -0,0 +1,16 @@
+import "./Form.css";
+
+const Form = () => {
+    return(
+        <section classname="Form">
+        <form>
+            <div>
+                <label for="firstname" classname="form-label"></label>
+                <input></input>
+            </div>
+        </form>
+        </section>
+    )
+}
+
+export default Form;
